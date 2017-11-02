0cfe99ecb080fb44c5a3638dafed8e6db0587b12
nicolas Loxol <nicolas.loxol@gmail.com>
2017-11-02 21:44:13 +0100

    Presentation de programme commente

--- TP2 - Resolution de systemes lineaires/tp2.c
+++ TP2 - Resolution de systemes lineaires/tp2.c
@@ -55,42 +55,7 @@ double** generer_matrice_de_moler(int n);
 int main()
 {
 	double **mat;
-
-	/*mat = allouer_memoire_matrice(3);
-
-	mat[0][0] = 1;
-	mat[0][1] = 1;
-	mat[0][2] = 1;
-	
-	mat[1][0] = 1;
-	mat[1][1] = 2;
-	mat[1][2] = 2;
-
-	mat[2][0] = 1;
-	mat[2][1] = 2;
-	mat[2][2] = 3;*/
-
 	mat = generer_matrice_kms(3, 0.5);
-
-	/*mat[0][0] = 4;
-	mat[0][1] = 1;
-	mat[0][2] = 1;
-	mat[0][3] = 0;
-
-	mat[1][0] = 1;
-	mat[1][1] = 4;
-	mat[1][2] = 0;
-	mat[1][3] = 1;
-
-	mat[2][0] = 1;
-	mat[2][1] = 0;
-	mat[2][2] = 4;
-	mat[2][3] = 1;
-
-	mat[3][0] = 0;
-	mat[3][1] = 1;
-	mat[3][2] = 1;
-	mat[3][3] = 4;*/
 
 	double second_membre[3];
 	double x[3];
@@ -101,16 +66,6 @@ int main()
 	x[0] = 0;
 	x[1] = 0;
 	x[2] = 0;
-
-	/*
-	second_membre[0] = 1;
-	second_membre[1] = 2;
-	second_membre[2] = 3;
-	second_membre[3] = 4;
-	x[0] = 0;
-	x[1] = 0;
-	x[2] = 0;
-	x[3] = 0;*/
 
 	tester_optimisation(mat, second_membre, x, 0.001, 3, 1024);
 

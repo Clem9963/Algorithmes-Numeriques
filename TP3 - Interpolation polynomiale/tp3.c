505ed74f697d9b3d708287a2c6f1e4507bdac6c7
Clement_Dif <clementdt99@gmail.com>
2017-11-14 09:17:37 +0100

    Modification du fichier tp3.c et ajout d'un dossier pour le TP4. Reste toujours à terminer Newton.

--- TP3 - Interpolation polynomiale/tp3.c
+++ TP3 - Interpolation polynomiale/tp3.c
@@ -4,15 +4,24 @@
 // PROTOTYPES
 
 double lagrange(double *x, double *y, int n, double a);
-double newton(double *x, double *y, int n, double a);
+double** calcule_differences_divisees(double *x, double *y, int n);
+void afficher_differences_divisees(int n, double **differences_divisees);
+void liberer_differences_divisees(int n, double **differences_divisees);
+double newton(int n, double **differences_divisees, double a);
 
 int main()
 {
-	double x[20] = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38};
+	/*double x[20] = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38};
 	double y[20] = {0.99987, 0.99997, 1, 0.99997, 0.99988, 0.99973, 0.99953, 0.99927, 0.99897, 0.99846,
-					0.99805, 0.99751, 0.99705, 0.99650, 0.99664, 0.99533, 0.99472, 0.99472, 0.99333, 0.99326};
+					0.99805, 0.99751, 0.99705, 0.99650, 0.99664, 0.99533, 0.99472, 0.99472, 0.99333, 0.99326};*/
 
-	printf("%f\n", lagrange(x, y, 20, 0));
+	double x[4] = {1, 2, 3, 4};
+	double y[4] = {0, 0, 0, 6};
+
+	double** differences_divisees = calcule_differences_divisees(x, y, 4);
+	newton(4, differences_divisees, 20);
+	liberer_differences_divisees(4, differences_divisees);
+
 	return 0;
 }
 
@@ -37,34 +46,80 @@ double lagrange(double *x, double *y, int n, double a)
 	return somme;
 }
 
-double newton(double *x, double *y, int n, double a)
+double** calcule_differences_divisees(double *x, double *y, int n)
 {
-	int a = 0;
-	int b = 0;
-	int ordre = 0;
-	double* differences_divisees[n-1] = {NULL};
-	for (a = 0; a < n-1; a++)
+	int i = 0;
+	int j = 0;
+	double** differences_divisees = calloc(n, sizeof(double*));
+	for (i = 0; i < n; i++)
 	{
-		differences_divisees[a] = calloc(n-1-a, sizeof(double));
+		differences_divisees[i] = calloc(n+1, sizeof(double));
 	}
-	for (a = 0; a < n-1; a++)
+
+	for (i = 0; i < n; i++)
 	{
-		for (b = 0; b < n-1-a; b++)
+		differences_divisees[i][0] = x[i];
+
+	}
+	for (i = 0; i < n; i++)
+	{
+		differences_divisees[i][1] = y[i];
+	}
+
+	for (j = 2; j < n+1; j++)
+	{
+		for (i = j-1; i < n; i++)
 		{
-			ordre = a + 1;
-			differences_divisees[a][b] =  /
-		}
+			differences_divisees[i][j] = (differences_divisees[i][j-1] - differences_divisees[j-2][j-1]) / (differences_divisees[i][0] - differences_divisees[j-2][0]);
+		} 
 	}
+	return differences_divisees;
 }
 
-double trouve_a(double **differences_divisees, double *a, int n)
+void afficher_differences_divisees(int n, double **differences_divisees)
 {
 	int i = 0;
 	int j = 0;
 	for (i = 0; i < n; i++)
 	{
-		a[i] = differences_divisees[i][0];
+		for (j = 0; j < n+1; j++)
+		{
+			printf("%.3f   ", differences_divisees[i][j]);
+		}
+		printf("\n");
 	}
 }
 
-double trouve_pa()
+void liberer_differences_divisees(int n, double **differences_divisees)
+{
+	int i = 0;
+	for (i = 0; i < n; i++)
+	{
+		free(differences_divisees[i]);
+	}
+	free(differences_divisees);
+}
+
+double newton(int n, double **differences_divisees, double a)
+{
+	double* tab_a = calloc(n, sizeof(double));
+	double pa = 0;
+	int i = 0;
+	int j = 1;
+	
+	for (i = 0; i < n; i++)
+	{
+		tab_a[i] = differences_divisees[i][j];
+		j++;
+		printf("%f   ", tab_a[i]);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (int i = 0; i < count; ++i)
+		{
+			/* code */
+		}
+		pa = (tab_a[i]);
+	}
+}
